ab5e873de6dd9e8436ea11c9456bb1d020282b76
sfreille <sebastian.freille@gmail.com>
2023-08-28 12:42:58 -0300

    update slides

--- epol/code/country-level-government-spending-taxes.R
+++ epol/code/country-level-government-spending-taxes.R
@@ -1,25 +1,31 @@
-dat <- read.csv("country-level-government-spending-vs-income.csv",header=TRUE)
-dat <- dat[dat$Code!="QAT",]
-dat <- dat[dat$Code!="KIR",]
-dat <- dat[dat$Code!="TUV",]
-dat2011 <- dat[dat$Year==2011,]
-dat2011  <- dat2011[dat2011$Population..historical.estimates.<1500000000,]
-dat2011  <- dat2011[dat2011$Population..historical.estimates.!="NA",]
+# Load libraries/packages
+lapply(c("rio","here","data.table","dplyr","ggplot","RcolorBrewer","viridis","areaplot","ggthemes","ggrepel"),require,character.only=TRUE)
 
+# Load data using relative hierarchy
+exp <- import(here("slides","epol","data","country-level-government-spending-vs-income.csv"),header=TRUE)
 
+tax <- import(here("slides","epol","data","country-level-taxes-vs-income.csv"),header=TRUE)
+
+# Clean data, drop cases
+exp <- exp[exp$Code!="QAT",]
+exp <- exp[exp$Code!="KIR",]
+exp <- exp[exp$Code!="TUV",]
+exp2011 <- exp[exp$Year==2011,]
+exp2011$`Population (historical estimates)`  <- as.numeric(exp2011$`Population (historical estimates)`)
+exp2011  <- exp2011[exp2011$`Population (historical estimates)`<1500000000,]
+
+tax2015 <- tax[tax$Year==2015,]
+tax2015$`Population (historical estimates)`  <- as.numeric(tax2015$`Population (historical estimates)`)
+tax2015  <- tax2015[tax2015$`Population (historical estimates)`<1500000000,]
+
+# Select countries to include in graph
 pointsToLabel <- c("Russia","Venezuela","Iraq","Myanmar","Sudan","Afghanistan","Congo","Greece","Argentina","Italy","South Africa", "Spain","Botswana", "Cape Verde", "Bhutan","Rwanda","France","United States","Germany","Britain","Barbados","Norway","Japan","New Zealand","Singapore","Uruguay","Chile","Turkey","Portugal","Peru","Mexico","Israel","Italy","Lesotho","South Korea","Hong Kong","Lybia","Saudi Arabia","Kuwait","Oman","China","India","Kenya","Brazil","Mexico","Qatar","United Arab Emirates","Ethiophia","Pakistan","Libya","Greece","Fiji","Egypt","Malawi","Mozambique","Burundi")
 
-png("fig01.png",width=1920,height=1080,res=225)
-ggplot(dat2011, aes(x=Government.Expenditure..IMF.based.on.Mauro.et.al...2015..,y=GDP.per.capita..PPP..constant.2017.international...)) + geom_point(colour="purple",alpha=0.5,aes(size=Population..historical.estimates.))+scale_size_continuous(range=c(1,30)) + geom_text_repel(size=4,aes(label=Entity), data=dat2011[dat2011$Entity %in% pointsToLabel,],min.segment.length = Inf)+theme(legend.position="none")+ ylab("PIB per capita (2010)")+xlab("Gasto publico como fraccion del PIB (2010)")+ theme(text=element_text(size=14),axis.text=element_text(size=14))+xlim(20,65)+scale_y_log10(breaks=c(1000,2000,5000,10000,20000,50000))
+# Graph and save
+png(filename="slides/epol/output/figs/fig01.png",width=1920,height=1080,res=225)
+ggplot(exp2011, aes(x=`Government Expenditure (IMF based on Mauro et al. (2015))`,y=`GDP per capita, PPP (constant 2017 international $)`)) + geom_point(colour="purple",alpha=0.5,na.rm=TRUE,aes(size=`Population (historical estimates)`))+scale_size_continuous(range=c(1,30)) + geom_text_repel(size=4,aes(label=Entity), data=exp2011[exp2011$Entity %in% pointsToLabel,],min.segment.length = Inf)+theme(legend.position="none")+ ylab("PIB per capita (2011)")+xlab("Gasto publico como fraccion del PIB (2011)")+ theme(text=element_text(size=14),axis.text=element_text(size=14))+xlim(20,65)+scale_y_log10(breaks=c(1000,2000,5000,10000,20000,50000))
 dev.off()
 
-
-dat2 <- read.csv("country-level-taxes-vs-income.csv",header=TRUE)
-dat22015 <- dat2[dat2$Year==2015,]
-
-dat22015  <- dat22015[dat22015$Population..historical.estimates.<1500000000,]
-dat22015  <- dat22015[dat22015$Population..historical.estimates.!="NA",]
-
-png("fig02.png",width=1920,height=1080,res=225)
-ggplot(dat22015, aes(x=Total.tax.revenue....of.GDP...ICTD..2021.., y=GDP.per.capita..PPP..constant.2017.international...)) + geom_point(colour="purple",alpha=0.5,aes(size=Population..historical.estimates.))+scale_size_continuous(range=c(1,30))+geom_text_repel(size=4,aes(label=Entity), data=dat22015[dat22015$Entity %in% pointsToLabel,],min.segment.length = Inf)+ylab("PIB per capita (2010)")+xlab("Recaudacion tributaria como fraccion del PIB (2010)")+ theme(text=element_text(size=14),axis.text=element_text(size=14))+xlim(20,50)+scale_y_log10(breaks=c(1000,2000,5000,10000,20000,50000))+theme(legend.position="none")
+png(filename="slides/epol/output/figs/fig02.png",width=1920,height=1080,res=225)
+ggplot(tax2015, aes(x=`Total tax revenue (% of GDP) (ICTD (2021))`, y=`GDP per capita, PPP (constant 2017 international $)`)) + geom_point(colour="purple",alpha=0.5,aes(size=`Population (historical estimates)`))+scale_size_continuous(range=c(1,30))+geom_text_repel(size=4,aes(label=Entity), data=tax2015[tax2015$Entity %in% pointsToLabel,],min.segment.length = Inf)+ylab("PIB per capita (2010)")+xlab("Recaudacion tributaria como fraccion del PIB (2010)")+ theme(text=element_text(size=14),axis.text=element_text(size=14))+xlim(20,50)+scale_y_log10(breaks=c(1000,2000,5000,10000,20000,50000))+theme(legend.position="none")
 dev.off()
--- epol/code/share-democracies-bmr.R
+++ epol/code/share-democracies-bmr.R
@@ -1,19 +1,13 @@
-library(rio)
-library(here)
-library(data.table)
-library(dplyr)
-library(tmap)
-library(sf)
-library(RColorBrewer)
-library(viridis)
-library(ggplot2)
-library(hrbrthemes)
+# Load libraries/packages
+lapply(c("rio","here","data.table","ggplot2","dplyr","tmap","sf","RcolorBrewer","viridis","ggthemes","hrbrthemes"),require,character.only=TRUE)
 
-sha  <-  rio::import(here("slides","epol","data","share-democracies-bmr.csv"))  %>% data.table()
+# Load data using relative hierarchy
+sha  <-  import(here("slides","epol","data","share-democracies-bmr.csv"))
 sha  <- sha[sha$Entity=="World",]
 sha  <- melt(sha,measure.vars=c("number_nondem_bmr_owid","number_dem_bmr_owid"),id=c("Entity","Year"),variable.factor=c("non dem","dem"))
 levels(sha$variable)  <- c("Non-democracies","Democracies")
 
-png("fig-03-002.png",width=1920,height=1080,res=225)
+# Graph and save
+png("slides/epol/output/figs/fig-03-002.png",width=1920,height=1080,res=225)
 ggplot(sha,aes(fill=as.factor(variable),x=Year,y=value))+geom_area(stat="identity",position="fill",alpha=0.8 , size=.6, colour="white")+scale_fill_viridis(begin=0.1,end=0.5,discrete = T)+theme_ipsum(grid="XY")+scale_x_continuous(expand=c(0,0)) +theme(axis.text.x=element_text(hjust=c(0, 0.5, 0.5, 0.5, 1))) +  theme(legend.title=element_blank(),legend.position="bottom")+labs(x="Year",y="Proportion of contries")
 dev.off()
